08e1fd10015d0b53911d267e8c38e09043ffd27e
iturkan6 <iturkan6@gmail.com>
2020-07-31 18:01:15 +0400

    #HackerRank task

--- src/BetweenTwoSets.java
+++ src/BetweenTwoSets.java
@@ -11,18 +11,20 @@ public class BetweenTwoSets {
         while (min > 0) {
             boolean mark = true;
             for (Integer el1 : b) {
-                if (el1 % min != 0) mark = false;
+                if (el1 % min != 0) {
+                    mark = false;
+                    break;
+                }
             }
             if (mark && !list.contains(min)) list.add(min);
             min--;
         }
-        new ArrayList<>(list).forEach(nums -> {
-            a.stream().filter(el2 -> nums % el2 != 0).map(el2 -> nums).forEach(list::remove);
-        });
+        new ArrayList<>(list).forEach(nums -> a.stream().filter(el2 -> nums % el2 != 0).map(el2 -> nums)
+                .forEach(list::remove));
         return list.size();
     }
 
     public static void main(String[] args) {
-        System.out.println(getTotalX(Arrays.asList(2), Arrays.asList(20, 30, 12)));
+        System.out.println(getTotalX(Collections.singletonList(2), Arrays.asList(20, 30, 12)));
     }
 }
